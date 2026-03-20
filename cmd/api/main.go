753b49991fe8963c30ca85ef669cc48970063817
unsubstantiated-Script <jusfulton@gmail.com>
2026-03-20 09:27:28 -0500

    Initial server startup and test endpoint

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+	"os"
+)
 
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte("ok"))
+	})
+
+	addr := ":" + port
+	log.Printf("api listening on %s", addr)
+	if err := http.ListenAndServe(addr, mux); err != nil {
+		log.Fatalf("server failed: %v", err)
+	}
 }
